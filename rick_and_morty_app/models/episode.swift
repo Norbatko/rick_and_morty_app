4419cfd7feb2befb487d97848ee0ec28e95af1b0
Tomáš Ebert <xebert@fi.muni.cz>
2025-02-12 19:36:04 +0100

    fix: Edit episode model for API

--- rick_and_morty_app/models/episode.swift
+++ rick_and_morty_app/models/episode.swift
@@ -5,11 +5,23 @@
 //  Created by Tomáš Ebert on 11.02.2025.
 //
 
+struct EpisodeResponse: Codable {
+    let info: Info
+    let results: [Episode]
+}
+
+struct Info: Codable {
+    let count: Int
+    let pages: Int
+    let next: String?
+    let prev: String?
+}
+
 struct Episode: Identifiable, Codable {
     let id: Int
     let name: String
     let air_date: String
-    let episode: Int
+    let episode: String
     let characters: [String]
     let url: String
     let created: String
