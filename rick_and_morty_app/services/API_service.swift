9d6a5d9e2d35b76e06b23a38386c1793c2f4c9b9
Tomáš Ebert <xebert@fi.muni.cz>
2025-02-14 20:06:10 +0100

    feat: Refactor API functions

--- rick_and_morty_app/services/API_service.swift
+++ rick_and_morty_app/services/API_service.swift
@@ -12,16 +12,14 @@ class APIService {
     
     private let baseURL = "https://rickandmortyapi.com/api"
     
-    func fetchEpisodes(completion: @escaping ([Episode]?) -> Void) {
-        let urlString = "https://rickandmortyapi.com/api/episode"
-        
-        guard let url = URL(string: urlString) else {
+    private func fetchData<T: Decodable>(from endpoint: String, completion: @escaping (T?) -> Void) {
+        guard let url = URL(string: "\(baseURL)/\(endpoint)") else {
             print("Invalid URL")
             completion(nil)
             return
         }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        URLSession.shared.dataTask(with: url) { data, _, error in
             if let error = error {
                 print("Error fetching data: \(error)")
                 completion(nil)
@@ -35,8 +33,8 @@ class APIService {
             }
             
             do {
-                let decodedResponse = try JSONDecoder().decode(EpisodeResponse.self, from: data)
-                completion(decodedResponse.results)
+                let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                completion(decodedResponse)
             } catch {
                 print("Decoding error: \(error)")
                 completion(nil)
@@ -44,36 +42,30 @@ class APIService {
         }.resume()
     }
     
-    func fetchCharacters(completion: @escaping ([Character]?) -> Void) {
-        let urlString = "https://rickandmortyapi.com/api/character"
-        
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            completion(nil)
-            return
+    func fetchEpisodes(completion: @escaping ([Episode]?) -> Void) {
+        fetchData(from: "episode") { (response: EpisodeResponse?) in
+            completion(response?.results)
         }
+    }
+    
+    func fetchEpisode(id: Int, completion: @escaping (Episode?) -> Void) {
+        fetchData(from: "episode/\(id)") { (response: Episode?) in
+            completion(response)
+        }
+    }
+    
+    func fetchCharacters(completion: @escaping ([Character]?) -> Void) {
+        fetchData(from: "character") { (response: CharacterResponse?) in
+            completion(response?.results)
+        }
+    }
+    
+    func fetchCharactersByIDs(characters: [String], completion: @escaping ([Character]?) -> Void) {
+        let ids = characters.compactMap { URL(string: $0)?.lastPathComponent }
+        let idsString = ids.map { String($0) }.joined(separator: ",")
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Error fetching data: \(error)")
-                completion(nil)
-                return
-            }
-            
-            guard let data = data else {
-                print("No data received")
-                completion(nil)
-                return
-            }
-            
-            do {
-                let decodedResponse = try JSONDecoder().decode(CharacterResponse.self, from: data)
-                completion(decodedResponse.results)
-            } catch {
-                print("Decoding error: \(error)")
-                completion(nil)
-            }
-            
-        }.resume()
+        fetchData(from: "character/\(idsString)") { (response: [Character]?) in
+            completion(response)
+        }
     }
 }
