dba69e72f9742e98b1411465101a157ebd0b4f23
Tomáš Ebert <xebert@fi.muni.cz>
2025-02-14 20:07:05 +0100

    feat: Add function for fetching only desire episode

--- rick_and_morty_app/viewmodels/episodes_viewmodel.swift
+++ rick_and_morty_app/viewmodels/episodes_viewmodel.swift
@@ -9,11 +9,20 @@ import Foundation
 
 class EpisodeViewModel: ObservableObject {
     @Published var episodes: [Episode]?
+    @Published var episode: Episode?
 
     func fetchEpisodes() {
         APIService.shared.fetchEpisodes { [weak self] episodes in
             DispatchQueue.main.async {
                 self?.episodes = episodes
+            }
+        }
+    }
+    
+    func fetchEpisode(episodeID: Int) {
+        APIService.shared.fetchEpisode(id: episodeID) { [weak self] episode in
+            DispatchQueue.main.async {
+                self?.episode = episode
             }
         }
     }
